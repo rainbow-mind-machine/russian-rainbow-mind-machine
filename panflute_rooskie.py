03841df3fc48db690f0f3152c2098d6dc493d6dd
Charles Reid <charlesreid1@gmail.com>
2018-05-07 01:39:40 -0700

    Add panflute pandoc filter to translate to Russian

---
+++ panflute_rooskie.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from panflute import *
+from google.cloud import translate
+
+
+def translate_ru(elem, doc):
+    if type(elem)==Para:
+        english = stringify(doc)
+        translate_client = translate.Client()
+        rooskie = translate_client.translate(
+                english,
+                target_language = 'ru')
+        rooskie = rooskie['translatedText']
+        return_para = []
+        for r in rooskie.split(" "):
+            return_para.append(Str(r))
+            return_para.append(Space)
+        return Para(*return_para)
+
+
+def main(doc=None):
+    return run_filter(translate_ru, doc=doc)
+
+
+if __name__ == "__main__":
+    main()

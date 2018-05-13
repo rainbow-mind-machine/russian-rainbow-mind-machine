10ef3c0b4778ca4222e3f2add4be9629272b2643
Charles Reid <charlesreid1@gmail.com>
2018-05-12 17:34:22 -0700

    fix ruskie filter to handle headers and link text

--- filters/ruskie.py
+++ filters/ruskie.py
@@ -4,10 +4,34 @@ import sys
 from google.cloud import translate
 from google.api_core.exceptions import Forbidden
 
+
+def english_to_russian(english):
+    """
+    Use Google Cloud Translate API to translate English to Russian
+    """
+    try:
+        translate_client = translate.Client()
+        ru = translate_client.translate(
+                english,
+                target_language='ru')
+        ru = ru['translatedText']
+        return ru
+
+    except Forbidden as e:
+        sys.stderr.write(str(e))
+        sys.stderr.write("Error calling Google Cloud Translate API\n")
+        sys.stderr.write("Continuing...\n")
+        pass
+
+
 def prepare(doc):
     doc.linklist = []
 
-def translate_ru(elem, doc):
+
+def translate_document(elem, doc):
+    """
+    Extract text from select elements and translate them
+    """
     if type(elem)==Para:
 
         # Walk it and look for links first
@@ -15,24 +39,15 @@ def translate_ru(elem, doc):
 
         english = stringify(elem)
 
-        sys.stderr.write("About to call Google Cloud Translate API\n")
-        sys.stderr.write("String length: %d\n"%(len(english)))
-        try:
-            translate_client = translate.Client()
-            rooskie = translate_client.translate(
-                    english,
-                    target_language = 'ru')
-            rooskie = rooskie['translatedText']
+        rooskie = english_to_russian(english)
+        new_elems = convert_text(rooskie,input_format='markdown')
+        return new_elems
 
-            new_elems = convert_text(rooskie,input_format='markdown')
-            return new_elems
-
-        except Forbidden as e:
-            sys.stderr.write(e)
-            sys.stderr.write("Error calling Google Cloud Translate API\n")
-            sys.stderr.write("Continuing...\n")
-            pass
-
+    elif type(elem)==Header:
+        english = stringify(elem)
+        rooskie = english_to_russian(english)
+        h = Header(Str(rooskie))
+        return h
 
 
 def strip_links(elem,doc):
@@ -43,23 +58,27 @@ def strip_links(elem,doc):
     if isinstance(elem,Link):
         doc.linklist.append(elem)
 
+
 def finalize(doc):
     """
     Create an unordered list at the end of the document
     containing all of the links.
     """
-    list_items = []
+    en = "Links"
+    ru = english_to_russian(en)
+    h = Header(Str(ru), level=1)
+
+    doc.content.insert(len(doc.content),h)
+
     for link in doc.linklist:
-        list_items.append(ListItem(Para(link)))
-    bullet_list = BulletList(*list_items)
-    # doc.content is a sequence of Element objects
-    # under the hood, that's just a ListContainer
-    # and the ListContainer is, under the hood, just a list
-    doc.content.insert(-1,bullet_list)
+        en = stringify(link)
+        ru = english_to_russian(en)
+        link2 = Link(Str(ru), url=link.url)
+        doc.content.insert(len(doc.content),Para(link2))
 
 
 def main(doc=None):
-    return run_filter(translate_ru, prepare, finalize, doc=doc)
+    return run_filter(translate_document, prepare, finalize, doc=doc)
 
 
 if __name__=="__main__":
